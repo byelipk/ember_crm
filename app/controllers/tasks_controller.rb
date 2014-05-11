c93f4e847580175b29393408a8bebaacacd866d8
byelipk <byelipk@gmail.com>
2014-05-10 22:35:18 -0400

    minor fixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   respond_to :json
 
   def index
-    @tasks = Task.find(params[:ids])
+    @tasks = params[:ids] ? Task.find(params[:ids]) : Task.all
     render json: @tasks, status: 200
   end
 
